9252914e258e84895d3d564b501b6c012945a902
Dmytro Bochorishvili <dbochorishvili@smartling.com>
2019-02-06 15:19:20 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var util = require('gulp-util')
+var util = require('gulp-util');
 var gulpConnect = require('gulp-connect');
 var connect = require('connect');
 var cors = require('cors');
@@ -10,41 +10,45 @@ var swaggerRepo = require('swagger-repo');
 
 var DIST_DIR = 'web_deploy';
 
-gulp.task('serve', ['build', 'watch', 'edit'], function() {
-  portfinder.getPort({port: 3000}, function (err, port) {
-    gulpConnect.server({
-      root: [DIST_DIR],
-      livereload: true,
-      port: port,
-      middleware: function (gulpConnect, opt) {
-        return [
-          cors()
-        ]
-      }
-    });
-  });
-});
-
-gulp.task('edit', function() {
+gulp.task('edit', function(done) {
   portfinder.getPort({port: 5000}, function (err, port) {
     var app = connect();
     app.use(swaggerRepo.swaggerEditorMiddleware());
     app.listen(port);
     util.log(util.colors.green('swagger-editor started http://localhost:' + port));
   });
+  done();
 });
 
-gulp.task('build', function (cb) {
+gulp.task('build', function (done) {
   exec('npm run build', function (err, stdout, stderr) {
     console.log(stderr);
-    cb(err);
+    done(err);
   });
 });
 
-gulp.task('reload', ['build'], function () {
-  gulp.src(DIST_DIR).pipe(gulpConnect.reload())
+gulp.task('reload', gulp.series('build', function (done) {
+  gulp.src(DIST_DIR).pipe(gulpConnect.reload());
+  done();
+}));
+
+gulp.task('watch', function (done) {
+  gulp.watch(['spec/**/*', 'web/**/*'], gulp.series('reload'));
+  done();
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['spec/**/*', 'web/**/*'], ['reload']);
-});
+gulp.task('serve', gulp.series('build', 'edit', 'watch', function() {
+    portfinder.getPort({port: 3000}, function (err, port) {
+        gulpConnect.server({
+            debug: true,
+            root: [DIST_DIR],
+            livereload: true,
+            port: port,
+            middleware: function (gulpConnect, opt) {
+                return [
+                    cors()
+                ]
+            }
+        });
+    });
+}));
