4ccf31e8305ce2f6e75d8e8f81c71f354c232b3b
Dmytro Bochorishvili <dbochorishvili@smartling.com>
2019-02-14 15:49:27 +0200

    fix branch preview script

--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -10,7 +10,7 @@ var branch = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH.toLowerCase(
 if (branch && branch !== 'gh-pages') {
   var branchPath = path.join('.tmp', 'preview', branch, '/');
   mkdir('-p', branchPath);
+  cp('-R', 'web/*', branchPath);
   exec('npm run swagger bundle -- -o ' + branchPath + 'swagger.json');
-  cp('web/index.html', branchPath);
   exec('deploy-to-gh-pages --update .tmp');
 }
