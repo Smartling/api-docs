c8b6a8b49c34bcf4ef061efc7c368a0520d2fefe
DmitryBochorishvili <dbochor@gmail.com>
2019-02-18 13:31:57 +0200

    Upgrade API documentation to OpenAPI specification 3.0 (#57)

--- scripts/build.js
+++ scripts/build.js
@@ -5,15 +5,15 @@ var Path = require('path');
 require('shelljs/global');
 set('-e');
 
-mkdir('-p', 'web_deploy')
+mkdir('-p', 'web_deploy');
 
 cp('-R', 'web/*', 'web_deploy/');
 
+exec('npm run swagger build  --        -o web_deploy');
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
-var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
-rm('-rf', 'web_deploy/swagger-ui/')
-cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
-sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
-
+var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui-dist'));
+rm('-rf', 'web_deploy/swagger-ui/');
+cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/');
+sed('-i', 'https://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html');
