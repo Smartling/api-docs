f1be5e158d055ae79f1b42e5e5a401f1bbde4497
Dmytro Bochorishvili <dbochorishvili@smartling.com>
2023-07-26 14:14:34 +0200

    remove swagger-ui

--- scripts/build.js
+++ scripts/build.js
@@ -13,10 +13,6 @@ exec('npm run swagger build  --        -o web_deploy');
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
-var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui-dist'));
-rm('-rf', 'web_deploy/swagger-ui/');
-cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/');
-sed('-i', 'https://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html');
 
 mkdir('-p', 'web_deploy/spec');
 cp('-R', 'spec/translation_quality', 'web_deploy/spec');
